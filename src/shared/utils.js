c4cf850ae74ac22c59692ab17fbe1999bc2f2b0f
DA <dragan.anatolie.m@gmail.com>
2025-07-03 17:27:31 +0300

    fix animations appear as skiped creating a stutter effect when the sort speed is set to highest value

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -10,7 +10,10 @@ export function randomize(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-export function delay(ms){
+export async function delay(ms){
+    // allow browser to render the frame before applying the delay
+    await new Promise(requestAnimationFrame);
+
     return new Promise((resolve) => {
         setTimeout(resolve, ms);
     })
